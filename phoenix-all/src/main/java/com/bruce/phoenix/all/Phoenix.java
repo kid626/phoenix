14250f81c105678d3236abeca9eb8835ea61cf80
fanzh626 <495802704@qq.com>
2024-05-18 10:44:23 +0800

    [function] 框架版本管理，可以通过 phoenix-bom 或者 phoenix-all 来进行依赖

---
+++ phoenix-all/src/main/java/com/bruce/phoenix/all/Phoenix.java
@@ -0,0 +1,20 @@
+package com.bruce.phoenix.all;
+
+import cn.hutool.core.lang.Console;
+
+/**
+ * @Copyright Copyright © 2024 fanzh . All rights reserved.
+ * @Desc
+ * @ProjectName phoenix
+ * @Date 2024/5/18 8:00
+ * @Author Bruce
+ */
+public class Phoenix {
+
+    public static final String AUTHOR = "Bruce";
+
+    private Phoenix() {
+        Console.log("Provided by Bruce");
+    }
+
+}
