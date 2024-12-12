3313db30ff0f998c275c371073273005658c2843
fanzh626 <495802704@qq.com>
2024-12-12 11:26:25 +0800

    [function] 添加通用树结构实体

---
+++ phoenix-common/src/main/java/com/bruce/phoenix/common/model/common/BaseTreeVO.java
@@ -0,0 +1,38 @@
+package com.bruce.phoenix.common.model.common;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @Copyright Copyright © 2024 fanzh . All rights reserved.
+ * @Desc
+ * @ProjectName phoenix
+ * @Date 2024/12/12 11:23
+ * @Author Bruce
+ */
+@Data
+@ApiModel("通用树结构实体")
+public class BaseTreeVO<T> {
+
+    @ApiModelProperty("编码")
+    private String code;
+
+    @ApiModelProperty("姓名")
+    private String name;
+
+    @ApiModelProperty("父编码")
+    private String pCode;
+
+    @ApiModelProperty("是否有子节点")
+    private boolean hasChildren;
+
+    @ApiModelProperty("额外信息")
+    private T ext;
+
+    @ApiModelProperty("子节点")
+    private List<BaseTreeVO<T>> child;
+
+}
