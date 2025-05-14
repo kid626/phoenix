7f90b596a2bc8439ec4af3b4314bbaed0a1b1200
fanzh626 <495802704@qq.com>
2025-05-14 15:21:14 +0800

    [function] 查询服务监控接口

---
+++ phoenix-sys/src/main/java/com/bruce/phoenix/sys/controller/SysServerController.java
@@ -0,0 +1,59 @@
+package com.bruce.phoenix.sys.controller;
+
+import cn.hutool.core.date.DateUtil;
+import com.alibaba.fastjson.JSONObject;
+import com.bruce.phoenix.sys.util.SystemUtil;
+import com.frame.common.base.api.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import oshi.SystemInfo;
+import oshi.hardware.HardwareAbstractionLayer;
+import oshi.software.os.OperatingSystem;
+
+import java.util.Date;
+
+/**
+ * @Copyright Copyright © 2025 fanzh . All rights reserved.
+ * @Desc
+ * @ProjectName phoenix
+ * @Date 2025/5/14 15:11
+ * @Author Bruce
+ */
+@RestController
+@RequestMapping("/sys/server")
+@Api(tags = "系统服务相关接口")
+@Slf4j
+public class SysServerController {
+
+    @GetMapping("/v1/monitor")
+    @ApiOperation("查询服务监控")
+    public Result<JSONObject> monitor() {
+        JSONObject result = new JSONObject(8);
+        try {
+            SystemInfo si = new SystemInfo();
+            OperatingSystem os = si.getOperatingSystem();
+            HardwareAbstractionLayer hal = si.getHardware();
+            // 系统信息
+            result.put("sys", SystemUtil.getSystemInfo(os));
+            // cpu 信息
+            result.put("cpu", SystemUtil.getCpuInfo(hal.getProcessor()));
+            // 内存信息
+            result.put("memory", SystemUtil.getMemoryInfo(hal.getMemory()));
+            // 交换区信息
+            result.put("swap", SystemUtil.getSwapInfo(hal.getMemory()));
+            // 磁盘
+            result.put("disk", SystemUtil.getDiskInfo(os));
+            result.put("time", DateUtil.format(new Date(), "HH:mm:ss"));
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            return Result.success();
+        }
+        return Result.success(result);
+    }
+
+
+}
---
+++ phoenix-sys/src/main/java/com/bruce/phoenix/sys/util/SystemUtil.java
@@ -0,0 +1,241 @@
+package com.bruce.phoenix.sys.util;
+
+import cn.hutool.core.date.BetweenFormatter;
+import cn.hutool.core.date.DateUtil;
+import com.alibaba.fastjson.JSONObject;
+import lombok.experimental.UtilityClass;
+import oshi.hardware.CentralProcessor;
+import oshi.hardware.GlobalMemory;
+import oshi.hardware.VirtualMemory;
+import oshi.software.os.FileSystem;
+import oshi.software.os.OSFileStore;
+import oshi.software.os.OperatingSystem;
+import oshi.util.FormatUtil;
+import oshi.util.Util;
+
+import java.lang.management.ManagementFactory;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.List;
+
+/**
+ * @Copyright Copyright © 2025 fanzh . All rights reserved.
+ * @Desc
+ * @ProjectName phoenix
+ * @Date 2025/5/14 15:16
+ * @Author Bruce
+ */
+@UtilityClass
+public class SystemUtil {
+
+    /**
+     * 定义GB的计算常量
+     */
+    private static final int GB = 1024 * 1024 * 1024;
+    /**
+     * 定义MB的计算常量
+     */
+    private static final int MB = 1024 * 1024;
+    /**
+     * 定义KB的计算常量
+     */
+    private static final int KB = 1024;
+    /**
+     * 格式化小数
+     */
+    private static final DecimalFormat DF = new DecimalFormat("0.00");
+
+    /**
+     * 获取磁盘信息
+     *
+     * @return /
+     */
+    public static JSONObject getDiskInfo(OperatingSystem os) {
+        JSONObject diskInfo = new JSONObject();
+        FileSystem fileSystem = os.getFileSystem();
+        List<OSFileStore> fsArray = fileSystem.getFileStores();
+        String osName = System.getProperty("os.name");
+        long available = 0, total = 0;
+        for (OSFileStore fs : fsArray) {
+            // windows 需要将所有磁盘分区累加，linux 和 mac 直接累加会出现磁盘重复的问题，待修复
+            if (osName.toLowerCase().startsWith("win")) {
+                available += fs.getUsableSpace();
+                total += fs.getTotalSpace();
+            } else {
+                available = fs.getUsableSpace();
+                total = fs.getTotalSpace();
+                break;
+            }
+        }
+        long used = total - available;
+        diskInfo.put("total", total > 0 ? getSize(total) : "?");
+        diskInfo.put("available", getSize(available));
+        diskInfo.put("used", getSize(used));
+        if (total != 0) {
+            diskInfo.put("usageRate", DF.format(used / (double) total * 100));
+        } else {
+            diskInfo.put("usageRate", 0);
+        }
+        return diskInfo;
+    }
+
+    /**
+     * 获取交换区信息
+     *
+     * @param memory /
+     * @return /
+     */
+    public static JSONObject getSwapInfo(GlobalMemory memory) {
+        JSONObject swapInfo = new JSONObject();
+        VirtualMemory virtualMemory = memory.getVirtualMemory();
+        long total = virtualMemory.getSwapTotal();
+        long used = virtualMemory.getSwapUsed();
+        swapInfo.put("total", FormatUtil.formatBytes(total));
+        swapInfo.put("used", FormatUtil.formatBytes(used));
+        swapInfo.put("available", FormatUtil.formatBytes(total - used));
+        if (used == 0) {
+            swapInfo.put("usageRate", 0);
+        } else {
+            swapInfo.put("usageRate", DF.format(used / (double) total * 100));
+        }
+        return swapInfo;
+    }
+
+    /**
+     * 获取内存信息
+     *
+     * @param memory /
+     * @return /
+     */
+    public static JSONObject getMemoryInfo(GlobalMemory memory) {
+        JSONObject memoryInfo = new JSONObject();
+        memoryInfo.put("total", FormatUtil.formatBytes(memory.getTotal()));
+        memoryInfo.put("available", FormatUtil.formatBytes(memory.getAvailable()));
+        memoryInfo.put("used", FormatUtil.formatBytes(memory.getTotal() - memory.getAvailable()));
+        memoryInfo.put("usageRate", DF.format((memory.getTotal() - memory.getAvailable()) / (double) memory.getTotal() * 100));
+        return memoryInfo;
+    }
+
+    /**
+     * 获取Cpu相关信息
+     *
+     * @param processor /
+     * @return /
+     */
+    public static JSONObject getCpuInfo(CentralProcessor processor) {
+        JSONObject cpuInfo = new JSONObject();
+        cpuInfo.put("name", processor.getProcessorIdentifier().getName());
+        cpuInfo.put("package", processor.getPhysicalPackageCount() + "个物理CPU");
+        cpuInfo.put("core", processor.getPhysicalProcessorCount() + "个物理核心");
+        cpuInfo.put("coreNumber", processor.getPhysicalProcessorCount());
+        cpuInfo.put("logic", processor.getLogicalProcessorCount() + "个逻辑CPU");
+        // CPU信息
+        long[] prevTicks = processor.getSystemCpuLoadTicks();
+        // 默认等待300毫秒...
+        long time = 300;
+        Util.sleep(time);
+        long[] ticks = processor.getSystemCpuLoadTicks();
+        while (Arrays.toString(prevTicks).equals(Arrays.toString(ticks)) && time < 1000) {
+            time += 25;
+            Util.sleep(25);
+            ticks = processor.getSystemCpuLoadTicks();
+        }
+        long user = ticks[CentralProcessor.TickType.USER.getIndex()] - prevTicks[CentralProcessor.TickType.USER.getIndex()];
+        long nice = ticks[CentralProcessor.TickType.NICE.getIndex()] - prevTicks[CentralProcessor.TickType.NICE.getIndex()];
+        long sys = ticks[CentralProcessor.TickType.SYSTEM.getIndex()] - prevTicks[CentralProcessor.TickType.SYSTEM.getIndex()];
+        long idle = ticks[CentralProcessor.TickType.IDLE.getIndex()] - prevTicks[CentralProcessor.TickType.IDLE.getIndex()];
+        long iowait = ticks[CentralProcessor.TickType.IOWAIT.getIndex()] - prevTicks[CentralProcessor.TickType.IOWAIT.getIndex()];
+        long irq = ticks[CentralProcessor.TickType.IRQ.getIndex()] - prevTicks[CentralProcessor.TickType.IRQ.getIndex()];
+        long softirq = ticks[CentralProcessor.TickType.SOFTIRQ.getIndex()] - prevTicks[CentralProcessor.TickType.SOFTIRQ.getIndex()];
+        long steal = ticks[CentralProcessor.TickType.STEAL.getIndex()] - prevTicks[CentralProcessor.TickType.STEAL.getIndex()];
+        long totalCpu = user + nice + sys + idle + iowait + irq + softirq + steal;
+        cpuInfo.put("used", DF.format(100d * user / totalCpu + 100d * sys / totalCpu));
+        cpuInfo.put("idle", DF.format(100d * idle / totalCpu));
+        return cpuInfo;
+    }
+
+    /**
+     * 获取系统相关信息,系统、运行天数、系统IP
+     *
+     * @param os /
+     * @return /
+     */
+    public static JSONObject getSystemInfo(OperatingSystem os) {
+        JSONObject systemInfo = new JSONObject();
+        // jvm 运行时间
+        long time = ManagementFactory.getRuntimeMXBean().getStartTime();
+        Date date = new Date(time);
+        // 计算项目运行时间
+        String formatBetween = DateUtil.formatBetween(date, new Date(), BetweenFormatter.Level.HOUR);
+        // 系统信息
+        systemInfo.put("os", os.toString());
+        systemInfo.put("day", formatBetween);
+        systemInfo.put("ip", getLocalIp());
+        return systemInfo;
+    }
+
+    /**
+     * 文件大小转换
+     */
+    private static String getSize(long size) {
+        String resultSize;
+        if (size / GB >= 1) {
+            //如果当前Byte的值大于等于1GB
+            resultSize = DF.format(size / (float) GB) + "GB   ";
+        } else if (size / MB >= 1) {
+            //如果当前Byte的值大于等于1MB
+            resultSize = DF.format(size / (float) MB) + "MB   ";
+        } else if (size / KB >= 1) {
+            //如果当前Byte的值大于等于1KB
+            resultSize = DF.format(size / (float) KB) + "KB   ";
+        } else {
+            resultSize = size + "B   ";
+        }
+        return resultSize;
+    }
+
+    /**
+     * 获取当前机器的IP
+     *
+     * @return /
+     */
+    private static String getLocalIp() {
+        try {
+            InetAddress candidateAddress = null;
+            // 遍历所有的网络接口
+            for (Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces(); interfaces.hasMoreElements(); ) {
+                NetworkInterface anInterface = interfaces.nextElement();
+                // 在所有的接口下再遍历IP
+                for (Enumeration<InetAddress> inetAddresses = anInterface.getInetAddresses(); inetAddresses.hasMoreElements(); ) {
+                    InetAddress inetAddr = inetAddresses.nextElement();
+                    // 排除loopback类型地址
+                    if (!inetAddr.isLoopbackAddress()) {
+                        if (inetAddr.isSiteLocalAddress()) {
+                            // 如果是site-local地址，就是它了
+                            return inetAddr.getHostAddress();
+                        } else if (candidateAddress == null) {
+                            // site-local类型的地址未被发现，先记录候选地址
+                            candidateAddress = inetAddr;
+                        }
+                    }
+                }
+            }
+            if (candidateAddress != null) {
+                return candidateAddress.getHostAddress();
+            }
+            // 如果没有发现 non-loopback地址.只能用最次选的方案
+            InetAddress jdkSuppliedAddress = InetAddress.getLocalHost();
+            if (jdkSuppliedAddress == null) {
+                return "";
+            }
+            return jdkSuppliedAddress.getHostAddress();
+        } catch (Exception e) {
+            return "";
+        }
+    }
+
+}
