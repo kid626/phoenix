3eb076aa4fcf33feab9d786a769f788252c083c6
fanzh626 <495802704@qq.com>
2025-07-10 15:14:34 +0800

    [function] 柱状图、折线图格式转换

--- phoenix-common/src/main/java/com/bruce/phoenix/common/model/screen/ScreenBarChartMapVO.java
+++ phoenix-common/src/main/java/com/bruce/phoenix/common/model/screen/ScreenBarChartMapVO.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Copyright Copyright © 2024 fanzh . All rights reserved.
@@ -18,16 +19,16 @@ import java.util.List;
  * @Author Bruce
  */
 @Data
-@ApiModel("折线图实体V2")
+@ApiModel("柱状图实体V3")
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class ScreenLineChartV2VO<T> implements Serializable {
+public class ScreenBarChartMapVO<T> implements Serializable {
 
     @ApiModelProperty("横坐标")
     private List<String> xAxis;
 
     @ApiModelProperty("纵坐标")
-    private List<ScreenBaseDataVO<T>> series;
+    private Map<String,List<T>> series;
 
 }
--- phoenix-common/src/main/java/com/bruce/phoenix/common/util/ScreenUtil.java
+++ phoenix-common/src/main/java/com/bruce/phoenix/common/util/ScreenUtil.java
@@ -4,9 +4,7 @@ import com.bruce.phoenix.common.model.enums.ScreenTypeEnum;
 import com.bruce.phoenix.common.model.screen.*;
 import lombok.experimental.UtilityClass;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * @Copyright Copyright © 2024 fanzh . All rights reserved.
@@ -24,41 +22,43 @@ public class ScreenUtil {
 
     public static <T> ScreenPieChartVO<T> buildPieChartVO(List<ScreenNameValueVO<T>> list, String name) {
         List<ScreenBaseDataVO<ScreenNameValueVO<T>>> series = new ArrayList<>();
-        ScreenBaseDataVO<ScreenNameValueVO<T>> screenBaseDataVO = ScreenBaseDataVO.<ScreenNameValueVO<T>>builder()
-                .type(ScreenTypeEnum.PIE.getCode())
-                .data(list)
-                .name(name)
-                .build();
+        ScreenBaseDataVO<ScreenNameValueVO<T>> screenBaseDataVO = ScreenBaseDataVO.<ScreenNameValueVO<T>>builder().type(ScreenTypeEnum.PIE.getCode()).data(list).name(name).build();
         series.add(screenBaseDataVO);
         return ScreenPieChartVO.<T>builder().series(series).build();
     }
 
     public static <T> ScreenBarChartVO<T> buildBarChartVO(List<ScreenBaseDataVO<T>> list, List<String> xAxis) {
-        return ScreenBarChartVO.<T>builder()
-                .series(Arrays.asList(list))
-                .xAxis(xAxis)
-                .build();
+        return ScreenBarChartVO.<T>builder().series(Arrays.asList(list)).xAxis(xAxis).build();
     }
 
     public static <T> ScreenBarChartV2VO<T> buildBarChartV2VO(List<ScreenBaseDataVO<T>> list, List<String> xAxis) {
-        return ScreenBarChartV2VO.<T>builder()
-                .series(list)
-                .xAxis(xAxis)
-                .build();
+        return ScreenBarChartV2VO.<T>builder().series(list).xAxis(xAxis).build();
     }
 
     public static <T> ScreenLineChartVO<T> buildLineChartVO(List<ScreenBaseDataVO<T>> list, List<String> xAxis) {
-        return ScreenLineChartVO.<T>builder()
-                .series(Arrays.asList(list))
-                .xAxis(xAxis)
-                .build();
+        return ScreenLineChartVO.<T>builder().series(Arrays.asList(list)).xAxis(xAxis).build();
     }
 
     public static <T> ScreenLineChartV2VO<T> buildLineChartV2VO(List<ScreenBaseDataVO<T>> list, List<String> xAxis) {
-        return ScreenLineChartV2VO.<T>builder()
-                .series(list)
-                .xAxis(xAxis)
-                .build();
+        return ScreenLineChartV2VO.<T>builder().series(list).xAxis(xAxis).build();
+    }
+
+    public static <T> ScreenLineChartMapVO<T> convert2LineChartMapVO(ScreenLineChartV2VO<T> vo) {
+        List<ScreenBaseDataVO<T>> series = vo.getSeries();
+        Map<String, List<T>> seriesMap = new HashMap<>();
+        for (ScreenBaseDataVO<T> tScreenBaseDataVO : series) {
+            seriesMap.put(tScreenBaseDataVO.getName(), tScreenBaseDataVO.getData());
+        }
+        return ScreenLineChartMapVO.<T>builder().series(seriesMap).xAxis(vo.getXAxis()).build();
+    }
+
+    public static <T> ScreenBarChartMapVO<T> convert2BarChartMapVO(ScreenBarChartV2VO<T> vo) {
+        List<ScreenBaseDataVO<T>> series = vo.getSeries();
+        Map<String, List<T>> seriesMap = new HashMap<>();
+        for (ScreenBaseDataVO<T> tScreenBaseDataVO : series) {
+            seriesMap.put(tScreenBaseDataVO.getName(), tScreenBaseDataVO.getData());
+        }
+        return ScreenBarChartMapVO.<T>builder().series(seriesMap).xAxis(vo.getXAxis()).build();
     }
 
 }
